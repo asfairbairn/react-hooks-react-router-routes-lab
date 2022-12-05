831eec5692888a21607b168fb791ff312fe580a5
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-12-05 16:40:05 -0600

    commit tree 076ad21ed53e4c4e57c457e6f804f369094c9616

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const createDirectorDiv = directors.map((director) => {
+    return (
+      <div key={director.name}>
+        <h2>{director.name}</h2>
+        <ul>
+          {director.movies.map((movie) => {
+            return <li key={movie}>{movie} </li> 
+          })}
+        </ul>
+      </div>
+    )
+  })
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {createDirectorDiv}
+
+    </div>
+  )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,27 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const createMovieDivs = movies.map((movie) => {
+    return (
+      <div key={movie.title}>
+        <h2>{movie.title}</h2>
+        <h4>Length: {movie.time} minutes</h4>
+        <ul>
+          {movie.genres.map((genre) => {
+            return <li key={genre}>{genre}</li>
+          })}
+        </ul>
+      </div>
+    )
+  })
+  return ( 
+    <div>
+      <h1>Movies Page</h1>
+      {createMovieDivs}
+
+
+    </div>
+  )
 }
 
 export default Movies;
